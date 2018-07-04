d8cfe9a40e7e5b8b6fa978cf93fa9bbeaf430c8b
brunoxd <bruno.mendes.costa@usp.br>
2018-07-04 00:51:17 -0300

    Add simpson's rule function

---
+++ simpson.m
@@ -0,0 +1,25 @@
+% Função que implementa a Regra de Integração Numérica Simpson 1/3 Composta.
+function integral = simpson(a = 0, b, n = 10000, func)
+	% Calcula o tamanho do passo.
+	h = (b - a)/n;
+
+	% Inicializa variáveis do somatório.
+	sumP = sumI = 0;	
+
+	% Indica que irá utilizar func como uma função f.
+	f = inline(func);	
+
+	% Calcula cada passo de f: x0, x1, ..., xn.
+	x = a:h:b;
+
+    % Calcula cada f(x) e realiza o somatório par e ímpar.
+    for i = 2:length(x)-1
+        sumP+= f(x(i)) * (1-mod(i,2));  
+		sumI+= f(x(i)) * mod(i,2); 
+    end
+
+	% Calcula o resultado final, segundo a regra de simpson 1/3 composta.	
+	integral = (h/3) * (f(a) + 4*sumI + 2*sumP + f(b));
+
+	return;
+endfunction
