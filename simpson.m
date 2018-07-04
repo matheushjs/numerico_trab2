2fd9a3d44d0bd8b2b95b476e32710da8551e9dfc
brunoxd <bruno.mendes.costa@usp.br>
2018-07-04 13:58:40 -0300

    Fix spaces

--- simpson.m
+++ simpson.m
@@ -12,11 +12,11 @@ function integral = simpson(a = 0, b, n = 10000, func)
 	% Calcula cada passo de f: x0, x1, ..., xn.
 	x = a:h:b;
 
-    % Calcula cada f(x) e realiza o somatório par e ímpar.
-    for i = 2:length(x)-1
+	% Calcula cada f(x) e realiza o somatório par e ímpar.
+	for i = 2:length(x)-1
 		sumP+= f(x(i)) * (1-mod(i,2));  
 		sumI+= f(x(i)) * mod(i,2); 
-    end
+	end
 
 	% Calcula o resultado final, segundo a regra de simpson 1/3 composta.	
 	integral = (h/3) * (f(a) + 4*sumI + 2*sumP + f(b));
